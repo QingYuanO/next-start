943bb26827c7dca7fe87e125cab15af89a8f8488
qingyuano <211720950@qq.com>
2024-09-08 10:05:49 +0800

    update: add suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
 import ThemeProvider from '@/components/ThemeProvider';
 
 import './globals.css';
@@ -13,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
